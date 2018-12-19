7b83738384f16e64dece53b7df346811e191037a
Kodi Neumiller <kodi@Udays-MacBook-Pro.local>
2018-12-19 11:35:07 -0700

    Moved where prefix is set

--- build-for-deb.sh
+++ build-for-deb.sh
@@ -2,11 +2,11 @@
 # Build the hyrax-dependencies binary tar ball for use with libdap and BES
 # debian builds.
 
+(prefix=/root/install)
+
 set -eu
 
 printenv
 
-
-(prefix=/root/install)
 (cd /root/hyrax-dependencies && make -j4 for-static-rpm)
 
